df6789d242ff1707b6f2f5025b2ea6a40e28f2b2
Nikolay Gorbachev <n1kko.gorbachevv@gmail.com>
2024-09-30 23:37:30 +0300

    Test FirstOrDefault method

--- LinqLearning/Program.cs
+++ LinqLearning/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace LinqLearning
+﻿using System.Linq;
+
+namespace LinqLearning
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Person> people = new List<Person>()
+            {
+                new Person("John", 18),
+                new Person("Alex", 20),
+                new Person("Maria", 36)
+            };
+
+            // Person? firstPerson = people.FirstOrDefault();
+            var firstPerson = people.First();
+            var lastPerson = people.LastOrDefault();
+
+            Console.WriteLine(firstPerson.Name);
+            Console.WriteLine(lastPerson);
         }
     }
 }
